1169181eb8fd813ee3968daabf9498e8ff7bb18d
ToqaAbdalla <132062106+Toqa781@users.noreply.github.com>
2023-11-01 20:46:10 +0200

    Create Task 1 q12.cpp

---
+++ Task 1 q12.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    map<string, int> phishingKeywords;
+    phishingKeywords["name"] = 1;
+    phishingKeywords["password"] = 3;
+    phishingKeywords["credit card"] = 2;
+    phishingKeywords["security"] = 3;
+    phishingKeywords["payment"]=2;
+    phishingKeywords["account"]=2;
+    phishingKeywords["alert"]=1;
+    phishingKeywords["bank"]=3;
+    phishingKeywords["click"]=2;
+    phishingKeywords["confirm"]=2;
+    phishingKeywords["deactivate"]=2;
+    phishingKeywords["email"]=1;
+    phishingKeywords["financial"]=2;
+    phishingKeywords["free"]=1;
+    phishingKeywords["important"]=2;
+    phishingKeywords["information"]=2;
+    phishingKeywords["login"]=2;
+    phishingKeywords["please"]=1;
+    phishingKeywords["suspicious"]=2;
+    phishingKeywords["urgent"]=2;
+    phishingKeywords["verify"]=2;
+    phishingKeywords["virus"]=2;
+    phishingKeywords["win"]=1;
+    phishingKeywords["your"]=1;
+    phishingKeywords["unusual"]=2;
+    phishingKeywords["update"]=2;
+    phishingKeywords["verification"]=2;
+    phishingKeywords["lottery"]=1;
+    phishingKeywords["inheritance"]=2;
+    phishingKeywords["confidential"]=3;
+    phishingKeywords["urgency"]=2;
+
+
+
+    cout<<"Enter the name of the text file: ";
+    string fileName;
+    cin>>fileName;
+    ifstream inputFile(fileName);
+    if (!inputFile) {
+        cout << "Failed to open the input file." << endl;
+        return 1;
+    }
+
+    string line;
+    int totalPoints = 0;
+
+    while (getline(inputFile, line)) {
+        int lineTotalPoints = 0;  // Total points for this line
+
+        for (auto& entry : phishingKeywords) {
+            string keyword = entry.first;
+            int pointVal = entry.second;
+            size_t found = line.find(keyword);
+            while (found != -1) {
+                lineTotalPoints += pointVal;
+                found = line.find(keyword, found + 1);
+            }
+        }
+
+        for (auto& entry : phishingKeywords) {
+            string keyword = entry.first;
+            int pointVal = entry.second;
+            size_t keywordCount = 0;
+            size_t pos = 0;
+
+            while ((pos = line.find(keyword, pos)) != string::npos) {
+                ++keywordCount;
+                pos += keyword.length();
+            }
+
+            if (keywordCount > 0) {
+                cout << "Keyword: " << keyword << ", Occurrences: " << keywordCount << ", Point Total: " << keywordCount * pointVal << endl;
+            }
+
+
+        }
+
+        totalPoints += lineTotalPoints;
+    }
+
+    inputFile.close();
+
+    cout << "Total Points: " << totalPoints << endl;
+
+    return 0;
+}
