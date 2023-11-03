288076ff86570d917a08331c4b109189361050b2
maihassan1 <114602980+maihassan1@users.noreply.github.com>
2023-11-03 17:29:51 +0200

    Update TASK 2.cpp

--- TASK 2.cpp
+++ TASK 2.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 class BigReal{
 private:
-   double value;
-   bool isValidReal(string& realNumber);
+    string value;
+    bool isValidReal(string realNumber);
 
 public:
-    BigReal(double realNumber=0.0);
-    BigReal(string& realNumber);
+    BigReal(double realNumber );
+    BigReal(string realNumber);
     BigReal(BigReal& other);
+    BigReal& operator=(BigReal& other);
 
     void setNum(string& realNumber);
     int size() const;
@@ -23,63 +24,85 @@ public:
 
     friend ostream& operator<<(ostream& out,BigReal& num);
 
-};
- bool BigReal::isValidReal(string &realNumber) {
-     if(realNumber.empty()){
-         return false;
-     }
-     int decimalPointCount=0;
-     for(int i=0;i<realNumber.size();i++){
-         if(!isdigit(realNumber[i])&& realNumber[i]!='.'
-         && realNumber[i]!='+'&& realNumber[i]!='-')
-             return false;
-         if(realNumber[i]=='.'){
-             decimalPointCount++;
-         }
-         if(decimalPointCount>1)
-             return false;
-     }
-     return true;
- }
- BigReal::BigReal(double realNumber):value(realNumber) {}
- BigReal::BigReal(string &realNumber) {
-     if(isValidReal(realNumber)){
-         value= stod(realNumber);
-     }
-     else
-         value=0.0;
- }
- BigReal::BigReal(BigReal &other) :value(other.value){}
 
- void BigReal::setNum(string &realNumber) {
-     if(isValidReal(realNumber)){
-         value= stod(realNumber);
-     }
-     else
-         value=0.0;
- }
- int BigReal::size() const {
-        string numString= to_string(abs(value));
-        int decimalPointPosition=numString.find('.');
-        if(decimalPointPosition!=-1){
-            return (numString.size())-1;
+};
+bool BigReal::isValidReal(string realNumber) {
+    if(realNumber.empty()){
+        return false;
+    }
+    int decimalPointCount=0;
+    for(int i=0;i<realNumber.size();i++){
+        if(!isdigit(realNumber[i])&& realNumber[i]!='.'
+           && realNumber[i]!='+'&& realNumber[i]!='-')
+            return false;
+        if(realNumber[i]=='.'){
+            decimalPointCount++;
         }
-        else
-            return (numString.size());
- }
- int BigReal::sign() const {
-     if(value<0)return -1;
-     else if(value>0)return 1;
-     else return 0;
- }
+        if(decimalPointCount>1)
+            return false;
+    }
+
+    return true;
+}
+BigReal::BigReal(double realNumber ) {
+
+    value = to_string(realNumber);
+    if(!isValidReal(value)){
+        value = "0.0";
+    }
+}
+BigReal::BigReal(string realNumber) {
+if(isValidReal(realNumber)){
+value=realNumber;
+}
+else
+value="0.0";
+}
+
+BigReal::BigReal(BigReal &other) :value(other.value){}
+BigReal& BigReal::operator=(BigReal& other){
+    if (this != &other) {
+        value = other.value;
+    }
+    return *this;
+}
+void BigReal::setNum(string &realNumber) {
+    if(isValidReal(realNumber)){
+        value= realNumber;
+    }
+    else
+        value="0.0";
+}
+int BigReal::size() const {
+
+        int decimalPointPosition = value.find('.');
+        if (decimalPointPosition != -1) {
+            return (value.size()) - 1;
+        } else
+            return (value.size());
+
+}
+int BigReal::sign() const {
+    if(value[0] == '-'){
+        return -1;
+    }
+    else if(value[0] == '+'){
+        return +1;
+    }
+    else return 0;
+}
 
 bool BigReal::operator==(BigReal &anotherReal) {
     return value==anotherReal.value;
- }
- ostream& operator<<(ostream& out,BigReal& num){
-     out<<num.value;
-     return out;
- }
- int main(){
-     
- }
+}
+ostream& operator<<(ostream& out,BigReal& num){
+    out<<num.value;
+    return out;
+}
+int main(){
+    BigReal r1("+3.3.3");
+    BigReal r2("-22");
+
+    cout<<r1.sign() <<" "<<r2.sign();
+
+}
