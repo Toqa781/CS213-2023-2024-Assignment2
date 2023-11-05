867f8e9654bb9105e10434ced25c371b5c973802
ToqaAbdalla <132062106+Toqa781@users.noreply.github.com>
2023-11-05 14:31:51 +0200

    Update Task1 q6-b.cpp

--- Task1 q6-b.cpp
+++ Task1 q6-b.cpp
@@ -1,3 +1,10 @@
+// File:Task1 question 6)b).cpp
+// Purpose:print the prefix followed by the all combinations of the suffix of k digits
+// Author:Toqa Abdalla Ahmed
+// Section:s7,8
+// ID:20220093
+// TA:Khaled Ibrahim
+// Date: 5 Nov 2023
 #include <bits/stdc++.h>
 using namespace std;
  void number(string prefix,int k){
