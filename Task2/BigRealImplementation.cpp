6a431c8bbda9a4ad9986a5a003c0923a59a314ec
ToqaAbdalla <132062106+Toqa781@users.noreply.github.com>
2023-11-05 14:52:00 +0200

    Update BigRealImplementation.cpp

--- Task2/BigRealImplementation.cpp
+++ Task2/BigRealImplementation.cpp
@@ -1,3 +1,10 @@
+// File:BigReal.h.cpp
+// Purpose:The implementation of class BigReal
+// Author:Toqa Abdalla Ahmed, Mai Hassan Awad, Farah
+// Section:s7,8
+// ID:20220093,20221172,20220453
+// TA:Khaled Ibrahim
+// Date: 5 Nov 2023
 #include <bits/stdc++.h>
 #include "BigReal.h"
 using namespace std;
