9737eb947dc944e9b804df5c7d3a4f6b5dc92bcd
ToqaAbdalla <132062106+Toqa781@users.noreply.github.com>
2023-11-14 11:40:38 +0200

    Update Task 3.cpp

--- Task 3.cpp
+++ Task 3.cpp
@@ -96,38 +96,100 @@ public:
             instructions.push_back(newHexa.substr(i,4));
         }
     }
-    void executeInstructions(){
-        for(int i=0;i<instructions.size();i++){
-            string x=instructions[i];
-            int dec_val=0;
-            int R=0;
-            int S=0;
-            int T=0;
-            if(x[0]=='1'){
-                if(x[3]>='0'&&x[3]<='9'){
-                    dec_val=x[3]-'0';
+    void executeInstructions() {
+        for (int i = 0; i < instructions.size(); i++) {
+            string x = instructions[i];
+            int dec_val = 0;
+            int R = 0;
+            int S = 0;
+            int T = 0;
+            if (x[0] == '1') {
+                // converting the last two characters from hexadecimal to decimal
+                if (x[3] >= '0' && x[3] <= '9') {
+                    dec_val = x[3] - '0';
+                } else if (x[3] >= 'A' && x[3] <= 'F') {
+                    dec_val = (int(x[3]) - 55);
                 }
-                else if(x[3]>='A'&& x[3]<='F'){
-                    dec_val=(int(x[3])-55);
-                }
-                if(x[2]>='0'&& x[2]<=9){
-                    dec_val+=(x[2]-'0')*16;
-                }
-                else if(x[2]>='A'&& x[2]<='F'){
-                    dec_val+=(int(x[2])-55)*16;
+                if (x[2] >= '0' && x[2] <= '9') {
+                    dec_val += (x[2] - '0') * 16;
+
+                } else if (x[2] >= 'A' && x[2] <= 'F') {
+                    dec_val += (int(x[2]) - 55) * 16;
                 }
                 if (x[1] >= '0' && x[1] <= '9') {
                     R = x[1] - '0';
                 } else if (x[1] >= 'A' && x[1] <= 'F') {
                     R = (int(x[1]) - 55);
                 }
-                int address=dec_val;
-                string dataMemory=memory.readMemory(address);
-                registers.writeRegister(R,dataMemory);
+                //loading data from a specific memory address into a specific register
+                int address = dec_val;
+                string datafrommemory = memory.readMemory(address);
+                registers.writeRegister(R, datafrommemory);
+
+            } else if (x[0] == '2') {
+
+
+            } else if (x[0] == '3') {
+
+            } else if (x[0] == '4') {
+
+
+            } else if (x[0] == '5') {
+                // converting the last 3 characters from hexadecimal to decimal
+                if (x[1] >= '0' && x[1] <= '9') {
+                    R = x[1] - '0';
+                } else if (x[1] >= 'A' && x[1] <= 'F') {
+                    R = (int(x[1]) - 55);
+                }
+                if (x[2] >= '0' && x[2] <= '9') {
+                    S = (x[2] - '0');
+
+                } else if (x[2] >= 'A' && x[2] <= 'F') {
+                    S = (int(x[2]) - 55);
+                }
+                if (x[3] >= '0' && x[3] <= '9') {
+                    T = x[3] - '0';
+                } else if (x[3] >= 'A' && x[3] <= 'F') {
+                    T = (int(x[3]) - 55);
+                }
+
+                //Adding the data in registers S and T then storing the sum in register R
+                string datafromS =  registers.readRegister(S) ;
+                string datafromT =  registers.readRegister(T) ;
+
+                //converting data inside registers S and T from hex to decimal
+                int dec1 = stoi(datafromS, nullptr, 16);
+                int dec2 = stoi(datafromT, nullptr, 16);
+
+                // add the two numbers
+                int sum = dec1 + dec2;
+
+                //convert the sum from decimal to hex
+                string hex = "";
+                for (int i = 0; i < 2; i++) {
+                    int remainder = sum % 16;
+                    if (remainder <= 9) {
+                        hex = char(remainder + '0') + hex;
+                    } else {
+                        hex = char(remainder + 55)  + hex;
+                    }
+                    sum = sum / 16;
+                }
+
+                //store the sum in register R
+                registers.writeRegister(R,hex);
+
+            } else if (x[0] == 'B') {
+
+
+            } else if (x[0] == 'C') {
+                return;
+
             }
         }
     }
 };
 int main() {
+
     return 0;
 }
